9bd49450db2d224285f85cc260852b4da22f0cef
NumerHero <469564715@qq.com>
2015-09-13 17:44:11 +0800

    new version

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,11 +95,12 @@ $(function()
           }
           return false;
         }
-
-
       }
+     
 });
+      
 
+ 
 function callback( data )
 {
   var oSearchInput = document.getElementById('search-input'),
@@ -272,17 +273,6 @@ function afterPjax() {
   // check();
   // container.scroll(check);
 
-  // 多说公共JS代码 start (一个网页只需插入一次) 
-  var duoshuoQuery = {short_name:"numerhero"};
-  (function() {
-    var ds = document.createElement('script');
-    ds.type = 'text/javascript';ds.async = true;
-    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-    ds.charset = 'UTF-8';
-    (document.getElementsByTagName('head')[0] 
-     || document.getElementsByTagName('body')[0]).appendChild(ds);
-  })();
-  // 多说公共JS代码 end
 }
 afterPjax();
 
