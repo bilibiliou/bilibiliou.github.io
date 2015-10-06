faf7f119474daca99595314a0aa369ed1849615e
NumerHero <469564715@qq.com>
2015-10-06 22:57:40 +0800

    new version

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,128 +3,101 @@
    @author 欧阳湘粤 (Owen)
    https://github.com/Numerhero/NumerHero.github.io
 \*---------------------------------*/
-  $(function(){
-    var oWeixin = document.querySelector("#weixin");
-    var oQR = document.querySelector("#QR-Code");
-    var oQrImg = oQR.getElementsByTagName('img')[0];
-    var l = $("#QR-Code").css("left"); 
-    var t = $("#QR-Code").css("top"); 
-    oWeixin.onmouseover = function()
-    {
-      $("#QR-Code").css("left",l);
-      $("#QR-Code").css("top",t);
-      $("#QR-Code").css("width",'160');
-      oQR.style.display = "block";
-      startMove(oQrImg , { width:150 , height :　150 , opacity:70})
-      startMove(oQR , { height :　160 , opacity:70});
-    }
 
-    oWeixin.onmouseout = function()
-    {
-        startMove(oQrImg , { width:0 , height :　0 ,  opacity:0} );
-        startMove(oQR , { width:0 , height :　0 ,  opacity:0 , top : 20 , left : 15 });   
-    }
-
-  });
-
-
-
-
-$(function()
+window.onload = function()
 {
-  var oSearchInput = document.getElementById('search-input'),
-      oSearchList  = document.getElementById('search-list'),
-      oSearchButton= document.getElementById('search-button'),
-      date         = new Date();
-      
-      oSearchInput.onkeyup = function()
-      {  
-        if( this.value != '' )
-        {
-          var oScript = document.createElement('script');
-          oScript.src = 'http://suggestion.baidu.com/su?wd='+ this.value + '&cb=callback' + '&t=' + date.getTime();
-          document.body.appendChild(oScript);
-        }else
-        {
-          oSearchList.style.display = 'none';
-          oSearchList.innerHTML = '';
-        }
-      }
-      oSearchButton.onclick = function()
+
+var oSearchInput = document.getElementById('search-input'),
+    oSearchList  = document.getElementById('search-list'),
+    oSearchButton= document.getElementById('search-button'),
+    date         = new Date();
+
+oSearchButton.onclick = function()
+{
+  window.open('http://www.baidu.com/s?wd=' + encodeURI(oSearchInput.value));
+  oSearchInput.value = '';
+}
+oSearchInput.onkeydown = function(ev)
+{
+  var oEvent = ev || event;
+  var index = 0;
+  var oLi = oSearchList.getElementsByTagName('li');
+  if( oEvent.keyCode == 13 )
+  {
+    window.open('http://www.baidu.com/s?wd=' + encodeURI(oSearchInput.value));
+    oSearchInput.value = '';
+  }
+
+  // Up Arrow
+  if( oEvent.keyCode == 38 && oSearchInput.value != '' && oSearchList.style.display == 'block' )
+  {
+    console.log('www');
+    if( index == 0 )
+    {
+      index = oLi.length - 1;
+    }
+    else
+    {
+        index++;  
+    }
+    console.log(oLi.length);
+    for(var i = 0; i<oLi.length ; i++)
+    {
+      if(i == index)
       {
-        window.open('http://www.baidu.com/s?wd=' + encodeURI(oSearchInput.value));
-        oSearchInput.value = '';
+        console.log(index);
+        oLi[i].className = 'search-onfoucs-li';
       }
-      oSearchInput.onkeydown = function(ev)
+      oLi[i].className = '';
+    }
+    return false;
+  }
+
+  // Down Arrow
+  if( oEvent.keyCode == 40 && oSearchInput.value != '' && oSearchList.style.display == 'block' )
+  { 
+    if( index == oLi.length - 1 )
+    {
+      index = 0;
+    }
+    else
+    {
+        index++; 
+    }
+    
+    for(var i = 0; i<oLi.length ; i++)
+    {
+      if(i == index)
       {
-        var oEvent = ev || event;
-        var index = -1;
-        var oLi = oSearchList.getElementsByTagName('li');
-        if( oEvent.keyCode == 13)
-        {
-          window.open('http://www.baidu.com/s?wd=' + encodeURI(oSearchInput.value));
-          oSearchInput.value = '';
-        }
-
-        if( oEvent.keyCode == 40 && oSearchInput.value != '' )
-        { 
-
-          if( index == oLi.length - 1)
-          {
-            index = 0;
-          }
-          else
-          {
-            index++;
-          }
-          if( index == -1 )
-          {
-            index = 0;
-          }
-          
-          for(var i = 0; i<oLi.length ; i++)
-          {
-            if(i == index)
-            {
-              oLi[i].style.background = '#9AFF9A';
-              oLi[i].style.color = 'white';
-            }
-            oLi[i].style.background = '';
-            oLi[i].style.color = '';
-          }
-          return false;
-        }
-
-        if( oEvent.keyCode == 38 && oSearchInput.value != '' )
-        {
-          if( index == 0)
-          {
-            index = oLi.length - 1;
-          }
-          else
-          {
-            index++;
-          }
-          if( index == -1 )
-          {
-            index = 0;
-          }
-          for(var i = 0; i<oLi.length ; i++)
-          {
-            if(i == index)
-            {
-              oLi[i].style.background = '#9AFF9A';
-              oLi[i].style.color = 'white';
-            }
-            oLi[i].style.background = '';
-            oLi[i].style.color = '';
-          }
-          return false;
-        }
+          oLi[i].className = 'search-onfoucs-li';
       }
-     
-});
-       
+      oLi[i].className = '';
+    }
+
+    return false;
+  }
+
+  
+}
+oSearchInput.onkeyup = function()
+{  
+  if( this.value != '' )
+  {
+    var oScript = document.createElement('script');
+    oScript.src = 'http://suggestion.baidu.com/su?wd='+ this.value + '&cb=callback' + '&t=' + date.getTime();
+    document.body.appendChild(oScript);
+  }else
+  {
+    oSearchList.style.display = 'none';
+    oSearchList.innerHTML = '';
+  }
+}
+
+
+
+
+}
+
 function callback( data )
 {
   var oSearchInput = document.getElementById('search-input'),
@@ -135,9 +108,10 @@ function callback( data )
   if( data.s.length )
   {
       oSearchList.style.display = 'block';
-      for(var i = 0 ; i<data.s.length ; i++)
+      html += '<li class="search-onfoucs-li">' + data.s[0] + '</li>'
+      for(var i = 1 ; i<data.s.length ; i++)
       {
-        html += '<li>' + data.s[i] + '</li>'
+        html += '<li class="">' + data.s[i] + '</li>'
       }
       oSearchList.innerHTML = html;
 
@@ -151,157 +125,204 @@ function callback( data )
         }
       }
 
+      for( var i = 0; i<oLi.length ; i++ )
+      {
+        oLi[i].onmouseover = function()
+        {
+          for(var i = 0 ; i<oLi.length ; i++)
+          {
+            oLi[i].className = '';
+          }
+          this.className = 'search-onfoucs-li';
+        }
+
+      }
+
   }else
   {
     oSearchList.style.display = 'none';
     oSearchList.innerHTML = '';
   }
 }
+$(function()
+{
+      function QEC(){
+          console.log('ss');
+          var oWeixin = document.querySelector("#weixin");
+          var oQR = document.querySelector("#QR-Code");
+          var oQrImg = oQR.getElementsByTagName('img')[0];
+          var l = $("#QR-Code").css("left"); 
+          var t = $("#QR-Code").css("top"); 
+          oWeixin.onmouseover = function()
+          {
+            $("#QR-Code").css("left",l);
+            $("#QR-Code").css("top",t);
+            $("#QR-Code").css("width",'160');
+            oQR.style.display = "block";
+            startMove(oQrImg , { width:150 , height :　150 , opacity:70})
+            startMove(oQR , { height :　160 , opacity:70});
+          }
 
-/*!--------------------------------*\
-   My blog Theme Peiwen Lu & Owen
-   
-   @Theme author Peiwen Lu (P233)
-   https://github.com/P233/3-Jekyll
-\*---------------------------------*/
-
-// Detect window size, if less than 1280px add class 'mobile' to sidebar therefore it will be auto hide when trigger the pjax request in small screen devices.
-if ($(window).width() <= 1280) {
-  $('#sidebar').addClass('mobile')
-}
-
-// Variables
-var sidebar    = $('#sidebar'),
-    container  = $('#post'),
-    content    = $('#pjax'),
-    button     = $('#icon-arrow');
-
-// Tags switcher
-var clickHandler = function(id) {
-  return function() {
-    $(this).addClass('active').siblings().removeClass('active');
-    $('.pl__all').hide();
-    $('.' + id).delay(50).fadeIn(350);
-  }
-};
-
-$('#tags__ul li').each(function(index){
-  $('#' + $(this).attr('id')).on('click', clickHandler($(this).attr('id')));
-});
-
-// If sidebar has class 'mobile', hide it after clicking.
-$('.pl__all').on('click', function() {
-  $(this).addClass('active').siblings().removeClass('active');
-  if (sidebar.hasClass('mobile')) {
-    $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
-  }
-});
-
-// Enable fullscreen.
-$('#js-fullscreen').on('click', function() {
-  if (button.hasClass('fullscreen')) {
-    sidebar.removeClass('fullscreen');
-    button.removeClass('fullscreen');
-    content.delay(300).queue(function(){
-      $(this).removeClass('fullscreen').dequeue();
-    });
-  } else {
-    sidebar.addClass('fullscreen');
-    button.addClass('fullscreen');
-    content.delay(200).queue(function(){
-      $(this).addClass('fullscreen').dequeue();
-    });
-  }
-});
-
-$('#mobile-avatar').on('click', function(){
-  $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
-});
-
-// Pjax
-$(document).pjax('#avatar, #mobile-avatar, .pl__all', '#pjax', { fragment: '#pjax', timeout: 10000 });
-$(document).on({
-  'pjax:click': function() {
-    content.removeClass('fadeIn').addClass('fadeOut');
-    NProgress.start();
-  },
-  'pjax:start': function() {
-    content.css({'opacity':0});
-  },
-  'pjax:end': function() {
-    NProgress.done();
-    container.scrollTop(0);
-    content.css({'opacity':1}).removeClass('fadeOut').addClass('fadeIn');
-    afterPjax();
-  }
-});
-
-// Re-run scripts for post content after pjax
-function afterPjax() {
-  // Open links in new tab
-  $('#post__content a').attr('target','_blank');
-
-  // Generate post TOC for h1 h2 and h3
-  var toc = $('#post__toc-ul');
-  // Empty TOC and generate an entry for h1
-  toc.empty().append('<li class="post__toc-li post__toc-h1"><a href="#post__title" class="js-anchor-link">' + $('#post__title').text() + '</a></li>');
-
-  // Generate entries for h2 and h3
-  $('#post__content').children('h2,h3').each(function() {
-    // Generate random ID for each heading
-    $(this).attr('id', function() {
-      var ID = "",
-          alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-      for(var i=0; i < 5; i++) {
-        ID += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+          oWeixin.onmouseout = function()
+          {
+              startMove(oQrImg , { width:0 , height :　0 ,  opacity:0} );
+              startMove(oQR , { width:0 , height :　0 ,  opacity:0 , top : 20 , left : 15 });   
+          }
       }
-      return ID;
-    });
+      QEC();
+      
+     
 
-    if ($(this).prop("tagName") == 'H2') {
-      toc.append('<li class="post__toc-li post__toc-h2"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
-    } else {
-      toc.append('<li class="post__toc-li post__toc-h3"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
-    }
-  });
+      /*!--------------------------------*\
+         My blog Theme Peiwen Lu & Owen
+         
+         @Theme author Peiwen Lu (P233)
+         https://github.com/P233/3-Jekyll
+      \*---------------------------------*/
 
-  // Smooth scrolling
-  $('.js-anchor-link').on('click', function() {
-    var target = $(this.hash);
-    container.animate({scrollTop: target.offset().top + container.scrollTop() - 70}, 500, function() {
-      target.addClass('flash').delay(700).queue(function() {
-        $(this).removeClass('flash').dequeue();
+      // Detect window size, if less than 1280px add class 'mobile' to sidebar therefore it will be auto hide when trigger the pjax request in small screen devices.
+      if ($(window).width() <= 1280) {
+        $('#sidebar').addClass('mobile')
+      }
+
+      // Variables
+      var sidebar    = $('#sidebar'),
+          container  = $('#post'),
+          content    = $('#pjax'),
+          button     = $('#icon-arrow');
+
+      // Tags switcher
+      var clickHandler = function(id) {
+        return function() {
+          $(this).addClass('active').siblings().removeClass('active');
+          $('.pl__all').hide();
+          $('.' + id).delay(50).fadeIn(350);
+        }
+      };
+      console.log(1);
+      $('#tags__ul li').each(function(index){
+        $('#' + $(this).attr('id')).on('click', clickHandler($(this).attr('id')));
       });
-    });
-  });
 
-  //加载Disqus 
-  // Lazy Loading Disqus
-  // http://jsfiddle.net/dragoncrew/SHGwe/1/
-  // var ds_loaded = false,
-  //     top = $('#disqus_thread').offset().top;
-  // window.disqus_shortname = $('#disqus_thread').attr('name');
+      // If sidebar has class 'mobile', hide it after clicking.
+      $('.pl__all').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+        if (sidebar.hasClass('mobile')) {
+          $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
+        }
+      });
 
-  // function check() {
-  //   if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
-  //     $.ajax({
-  //       type: 'GET',
-  //       url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-  //       dataType: 'script',
-  //       cache: true
-  //     });
-  //     ds_loaded = true;
-  //   }
-  // }
-  // check();
-  // container.scroll(check);
+      // Enable fullscreen.
+      $('#js-fullscreen').on('click', function() {
+        if (button.hasClass('fullscreen')) {
+          sidebar.removeClass('fullscreen');
+          button.removeClass('fullscreen');
+          content.delay(300).queue(function(){
+            $(this).removeClass('fullscreen').dequeue();
+          });
+        } else {
+          sidebar.addClass('fullscreen');
+          button.addClass('fullscreen');
+          content.delay(200).queue(function(){
+            $(this).addClass('fullscreen').dequeue();
+          });
+        }
+      });
 
-  // var dsThreadLikeText = document.querySelectorAll('.ds-thread-like-text')[0];
-  // console.log(dsThreadLikeText);
-  // (function(){
-  //   dsThreadLikeText.innerHTML = '喜欢就戳戳吧';
-  // })();
-}
-afterPjax();
+      $('#mobile-avatar').on('click', function(){
+        $('#sidebar, #pjax, #icon-arrow').addClass('fullscreen');
+      });
+
+      // Pjax
+      $(document).pjax('#avatar, #mobile-avatar, .pl__all', '#pjax', { fragment: '#pjax', timeout: 10000 });
+      $(document).on({
+        'pjax:click': function() {
+          content.removeClass('fadeIn').addClass('fadeOut');
+          NProgress.start();
+        },
+        'pjax:start': function() {
+          content.css({'opacity':0});
+        },
+        'pjax:end': function() {
+          NProgress.done();
+          container.scrollTop(0);
+          content.css({'opacity':1}).removeClass('fadeOut').addClass('fadeIn');
+          afterPjax();
+        }
+      });
+
+      // Re-run scripts for post content after pjax
+      function afterPjax() {
+        // Open links in new tab
+        $('#post__content a').attr('target','_blank');
+
+        // Generate post TOC for h1 h2 and h3
+        var toc = $('#post__toc-ul');
+        // Empty TOC and generate an entry for h1
+        toc.empty().append('<li class="post__toc-li post__toc-h1"><a href="#post__title" class="js-anchor-link">' + $('#post__title').text() + '</a></li>');
+
+        // Generate entries for h2 and h3
+        $('#post__content').children('h2,h3').each(function() {
+          // Generate random ID for each heading
+          $(this).attr('id', function() {
+            var ID = "",
+                alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            for(var i=0; i < 5; i++) {
+              ID += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            }
+            return ID;
+          });
+
+          if ($(this).prop("tagName") == 'H2') {
+            toc.append('<li class="post__toc-li post__toc-h2"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
+          } else {
+            toc.append('<li class="post__toc-li post__toc-h3"><a href="#' + $(this).attr('id') + '" class="js-anchor-link">' + $(this).text() + '</a></li>');
+          }
+        });
+
+        // Smooth scrolling
+        $('.js-anchor-link').on('click', function() {
+          var target = $(this.hash);
+          container.animate({scrollTop: target.offset().top + container.scrollTop() - 70}, 500, function() {
+            target.addClass('flash').delay(700).queue(function() {
+              $(this).removeClass('flash').dequeue();
+            });
+          });
+        });
+
+        //加载Disqus 
+        // Lazy Loading Disqus
+        // http://jsfiddle.net/dragoncrew/SHGwe/1/
+        // var ds_loaded = false,
+        //     top = $('#disqus_thread').offset().top;
+        // window.disqus_shortname = $('#disqus_thread').attr('name');
+
+        // function check() {
+        //   if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
+        //     $.ajax({
+        //       type: 'GET',
+        //       url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
+        //       dataType: 'script',
+        //       cache: true
+        //     });
+        //     ds_loaded = true;
+        //   }
+        // }
+        // check();
+        // container.scroll(check);
+
+        // var dsThreadLikeText = document.querySelectorAll('.ds-thread-like-text')[0];
+        // console.log(dsThreadLikeText);
+        // (function(){
+        //   dsThreadLikeText.innerHTML = '喜欢就戳戳吧';
+        // })();
+      }
+      afterPjax();
+
+
+     
+});
+       
+
 
