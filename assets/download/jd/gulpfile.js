8eac06a89183279c4e52e7ec880bf6b48b2d5ef6
numerhero <469564715@qq.com>
2016-07-26 16:37:12 +0800

    new version

--- assets/download/jd/gulpfile.js
+++ assets/download/jd/gulpfile.js
@@ -3,20 +3,27 @@ var gulp      = require("gulp"),
     uglify    = require("gulp-uglify")
     concat    = require("gulp-concat"),
     rename    = require("gulp-rename"),
-    notify    = require("gulp-notify")
+    notify    = require("gulp-notify"),
+    autoprefixer = require("gulp-autoprefixer"),
+    plumber   = require("gulp-plumber");
 
 
 gulp.task("style", function () {
     return gulp.src("./css/*.css")
+           .pipe(plumber())
            .pipe(concat("main.css"))
            .pipe(rename({suffix: ".min"}))
+           .pipe(autoprefixer({
+                browsers: ['last 3 version']
+            }))
            .pipe(minifyCss())
            .pipe(gulp.dest("./dist/css"))
            .pipe(notify("css 压缩已经完成"))
 })
 
 gulp.task("js", function () {
-    return gulp.src("./js/*js")
+    return gulp.src("./js/*.js")
+           .pipe(plumber())
            .pipe(concat("main.js"))
            .pipe(rename({suffix: ".min"}))
            .pipe(uglify())
